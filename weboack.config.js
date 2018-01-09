1b60a8cbc21f8c9693d89cda2c371aeae719b97d
szy0syz <szy0syz@gmail.com>
2018-01-09 22:31:42 +0800

    update 添加webpack.config.js文件

---
+++ weboack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const htmlPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {},
+  output: {},
+  module: {},
+  plugins: [],
+  devServer: {}
+}
