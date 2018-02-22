d2bec9a0198d18dbf6c1f6ea71838c28204785c8
szy0syz <szy0syz@gmail.com>
2018-02-22 23:05:14 +0800

    add pug

--- config/express.js
+++ config/express.js
@@ -1,13 +1,15 @@
 'use strict'
 
-import express    from 'express'
-import winston    from 'winston'
+import express from 'express'
+// import winston    from 'winston'
 
 const app = express()
 app.use(express.static('./public'))
+app.set('view engine', 'pug')
+app.set('views', './views')
 
 app.use((err, req, res, next) => {
   res.send('找不到此路由')
 })
 
-export default app;
+export default app
