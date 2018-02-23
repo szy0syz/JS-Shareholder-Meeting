ab86712fb023d52c602b2498313e6fb41a4f8071
szy0syz <szy0syz@gmail.com>
2018-02-23 23:25:29 +0800

    update 改造项目主文件app.js 重构成类模式

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+import R from 'ramda'
+import { resolve } from 'path'
 import express from './config/express'
 const config = require('./config')
+
+// 马上就要改咯~
 const db = require('./middlewares/database')
 
-const app = express
-db.database(app)
+// 合并绝对路径的
+const r = path => resolve(__dirname, path)
+// 声明需要加载的中间件
+const MIDDLEWARES = ['database']
 
-app.listen(config.port, () => {
-  console.log('Server started,', 'listening on port: ', config.port)
-})
+class APP {
+  constructor() {
+    this.app = express
+    db.database(this.app)
+  }
+
+  async start() {
+    this.app.listen(config.port, () => {
+      console.log('Server started,', 'listening on port: ', config.port)
+    })
+  }
+}
+
+const app = new APP()
+
+app.start()
