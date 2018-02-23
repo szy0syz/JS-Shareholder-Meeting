da7961bb743659b474b479675133adccb3936d1b
szy0syz <szy0syz@gmail.com>
2018-02-23 21:18:12 +0800

    update 增加start.js脚本支持ES6+

---
+++ start.js
@@ -0,0 +1,12 @@
+require('babel-core/register')({
+  'ignore': [/node_modules/],
+  'presets': [
+    'stage-3',
+    ['latest-node', {
+      'target': 'current'
+    }]
+  ]
+})
+
+require('babel-polyfill')
+require('./app')
