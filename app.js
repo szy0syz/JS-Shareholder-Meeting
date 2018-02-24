01cb69834f0439b6fa17086fed2c57ec940cf82a
szy0syz <szy0syz@gmail.com>
2018-02-24 21:52:42 +0800

    update 重构config-express到start.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import R from 'ramda'
 import { resolve } from 'path'
-import express from './config/express'
+import express from 'express'
+import bodyParser from 'body-parser'
 const config = require('./config')
 
 const r = path => resolve(__dirname, path)
@@ -9,7 +10,11 @@ const MIDDLEWARES = config.middlewares
 
 class APP {
   constructor() {
-    this.app = express
+    this.app = express()
+    this.app.use(bodyParser.json())
+    this.app.set('views', './views')
+    this.app.set('view engine', 'pug')
+    this.app.use(express.static('./public'))
     this.useMiddlewares(this.app)(MIDDLEWARES)
   }
 
